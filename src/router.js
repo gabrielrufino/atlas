75d55a101493e15a24d0a79587fd427d50be98fe
Gabriel Rufino <email@gabrielrufino.com>
2018-12-27 06:03:42 -0300

    Todas as rotas iniciais no router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,25 @@
 import VueRouter from 'vue-router'
+import Home from './views/Home'
+import RecognizePicture from './views/RecognizePicture'
+import About from './views/About'
 
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/sobre',
+    component: About
+  },
+  {
+    path: '/reconhecer-imagem',
+    component: RecognizePicture
+  }
+]
 
 const router = new VueRouter({
-  
+  routes
 })
+
+export default router

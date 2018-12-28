709f6158d62d70b3b693217d6ff420fe07c869a5
Gabriel Rufino <email@gabrielrufino.com>
2018-12-28 02:15:20 -0300

    cards

--- src/views/RecognizePicture.vue
+++ src/views/RecognizePicture.vue
@@ -1,86 +1,94 @@
 <template>
   <v-container>
-    <v-tabs
-      centered
-      light
-      icons-and-text
-    >
-      <v-tabs-slider color="primary"></v-tabs-slider>
+    <v-card>
+      <v-container>
+        <v-tabs
+          centered
+          light
+          icons-and-text
+        >
+          <v-tabs-slider color="primary"></v-tabs-slider>
 
-      <!-- Tabs -->
-      <v-tab>
-        Web
-        <v-icon>language</v-icon>
-      </v-tab>
+          <!-- Tabs -->
+          <v-tab>
+            Web
+            <v-icon>language</v-icon>
+          </v-tab>
 
-      <v-tab>
-        Upload
-        <v-icon>computer</v-icon>
-      </v-tab>
+          <v-tab>
+            Upload
+            <v-icon>computer</v-icon>
+          </v-tab>
 
-      <v-tab>
-        Câmera
-        <v-icon>camera_alt</v-icon>
-      </v-tab>
+          <v-tab>
+            Câmera
+            <v-icon>camera_alt</v-icon>
+          </v-tab>
 
-      <!-- Tabs content -->
-      <v-tab-item>
-        <v-layout row wrap>
-          <v-flex xs9>
-            <v-text-field
-              prepend-icon="link"
-              label="Insira a URL da imagem"
-              v-model="urlImage"
-            >
-            </v-text-field>
-          </v-flex>
-          <v-flex xs1></v-flex>
-          <v-flex xs2>
+          <!-- Tabs content -->
+          <v-tab-item>
+            <v-layout row wrap>
+              <v-flex xs9>
+                <v-text-field
+                  prepend-icon="link"
+                  label="Insira a URL da imagem"
+                  v-model="urlImage"
+                >
+                </v-text-field>
+              </v-flex>
+              <v-flex xs1></v-flex>
+              <v-flex xs2>
+                <v-btn
+                  color="primary"
+                  block
+                  large
+                  @click="loadImageFromUrl"
+                >
+                  Carregar
+                </v-btn>
+              </v-flex>
+            </v-layout>
+          </v-tab-item>
+
+          <v-tab-item>
+            <input id="image-upload" type="file" hidden @change="onFileChange">
             <v-btn
               color="primary"
-              block
-              large
-              @click="loadImageFromUrl"
+              class="white--text"
+              @click.native="openFileDialog"
             >
-              Carregar
+              Upload <v-icon right dark>cloud_upload</v-icon>
             </v-btn>
-          </v-flex>
-        </v-layout>
-      </v-tab-item>
+          </v-tab-item>
 
-      <v-tab-item>
-        <input id="image-upload" type="file" hidden @change="onFileChange">
-        <v-btn
-          color="primary"
-          class="white--text"
-          @click.native="openFileDialog"
-        >
-          Upload <v-icon right dark>cloud_upload</v-icon>
-        </v-btn>
-      </v-tab-item>
+          <v-tab-item>
+            Em breve...
+          </v-tab-item>
+        </v-tabs>
+      </v-container>
+    </v-card>
 
-      <v-tab-item>
-        Em breve...
-      </v-tab-item>
-    </v-tabs>
+    <v-divider class="my-3"></v-divider>
 
     <v-card>
-      <v-img
-        :src="image"
-        trasition
-      >
-      </v-img>
-      <v-btn
-        color="primary"
-        @click="classifyImage"
-        :loading="classifyingImage"
-        :disabled="classifyingImage"
-        block
-      >
-        Classificar imagem
-      </v-btn>
+      <v-container>
+        <v-img
+          :src="image"
+          trasition
+        >
+        </v-img>
+        <v-btn
+          color="primary"
+          @click="classifyImage"
+          :loading="classifyingImage"
+          :disabled="classifyingImage"
+          block
+        >
+          Classificar imagem
+        </v-btn>
 
-      <canvas id="myChart"></canvas>
+        <canvas id="myChart"></canvas>
+      </v-container>
     </v-card>
   </v-container>
 </template>

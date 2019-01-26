58d1b531815142aa4a63871bded60f6a733e88d9
Gabriel Rufino <>
2019-01-26 10:20:44 -0300

    state navOpened and mutations

--- src/store.js
+++ src/store.js
@@ -3,3 +3,22 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    navOpened: false
+  },
+  mutations: {
+    openNav (state) {
+      state.navOpened = true
+    },
+    closeNav (state) {
+      state.navOpened = false
+    },
+    moveNav (state) {
+      state.navOpened = !state.navOpened
+    }
+  }
+})
+
+export default store

6e13af3d9f16d981fd213d9d1a8ff0629bdbe0dc
Gabriel Rufino <>
2019-01-27 13:52:38 -0300

    Up in design

--- src/views/RecognizePicture.vue
+++ src/views/RecognizePicture.vue
@@ -2,97 +2,98 @@
   <v-container>
     <loader v-if="loadingClassifier"></loader>
     <div v-else>
-      <v-card>
-        <v-container>
-          <v-tabs
-            centered
-            light
-            icons-and-text
-          >
-            <v-tabs-slider color="primary"></v-tabs-slider>
+      <v-layout>
+        <v-flex md6>
+          <v-card>
+            <v-tabs
+              centered
+              light
+              icons-and-text
+            >
+              <v-tabs-slider color="primary"></v-tabs-slider>
 
-            <!-- Tabs -->
-            <v-tab>
-              Web
-              <v-icon>language</v-icon>
-            </v-tab>
+              <!-- Tabs -->
+              <v-tab>
+                Web
+                <v-icon>language</v-icon>
+              </v-tab>
 
-            <v-tab>
-              Upload
-              <v-icon>computer</v-icon>
-            </v-tab>
+              <v-tab>
+                Upload
+                <v-icon>computer</v-icon>
+              </v-tab>
 
-            <v-tab>
-              Câmera
-              <v-icon>camera_alt</v-icon>
-            </v-tab>
+              <v-tab>
+                Câmera
+                <v-icon>camera_alt</v-icon>
+              </v-tab>
 
-            <!-- Tabs content -->
-            <v-tab-item>
-              <v-layout row wrap>
-                <v-flex xs9>
-                  <v-text-field
-                    prepend-icon="link"
-                    label="Insira a URL da imagem"
-                    v-model="urlImage"
-                  >
-                  </v-text-field>
-                </v-flex>
-                <v-flex xs1></v-flex>
-                <v-flex xs2>
-                  <v-btn
-                    color="primary"
-                    block
-                    large
-                    @click="loadImageFromUrl"
-                  >
-                    Carregar
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-            </v-tab-item>
+              <!-- Tabs content -->
+              <v-tab-item>
+                <v-layout row wrap>
+                  <v-flex xs9>
+                    <v-text-field
+                      prepend-icon="link"
+                      label="Insira a URL da imagem"
+                      v-model="urlImage"
+                    >
+                    </v-text-field>
+                  </v-flex>
+                  <v-flex xs1></v-flex>
+                  <v-flex xs2>
+                    <v-btn
+                      color="primary"
+                      block
+                      large
+                      @click="loadImageFromUrl"
+                    >
+                      Carregar
+                    </v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-tab-item>
 
-            <v-tab-item>
-              <input id="image-upload" type="file" hidden @change="onFileChange">
+              <v-tab-item>
+                <input id="image-upload" type="file" hidden @change="onFileChange">
+                <v-btn
+                  color="primary"
+                  class="white--text"
+                  @click.native="openFileDialog"
+                >
+                  Upload <v-icon right dark>cloud_upload</v-icon>
+                </v-btn>
+              </v-tab-item>
+
+              <v-tab-item>
+                Em breve...
+              </v-tab-item>
+            </v-tabs>
+            <v-card-media>
+              <v-img
+                id="image"
+                :src="image"
+                trasition
+              ></v-img>
+            </v-card-media>
+
+            <v-card-actions>
               <v-btn
                 color="primary"
-                class="white--text"
-                @click.native="openFileDialog"
+                @click="classifyImage"
+                :loading="classifyingImage"
+                :disabled="classifyingImage"
+                block
               >
-                Upload <v-icon right dark>cloud_upload</v-icon>
+                Classificar imagem
               </v-btn>
-            </v-tab-item>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
 
-            <v-tab-item>
-              Em breve...
-            </v-tab-item>
-          </v-tabs>
-          <v-card-media>
-            <v-img
-              id="image"
-              :src="image"
-              trasition
-            ></v-img>
-          </v-card-media>
-        </v-container>
-      </v-card>
-
-      <v-divider class="my-3"></v-divider>
-
-      <v-card>
-        <v-container>
-          <v-btn
-            color="primary"
-            @click="classifyImage"
-            :loading="classifyingImage"
-            :disabled="classifyingImage"
-            block
-          >
-            Classificar imagem
-          </v-btn>
+        <v-flex md6>
           <bar :chart-data="chartData"></bar>
-        </v-container>
-      </v-card>
+        </v-flex>
+      </v-layout>
     </div>
   </v-container>
 </template>

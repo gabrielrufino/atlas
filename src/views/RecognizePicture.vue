d0d8c9474a521176f1f6d4ad03f1133741cdff6e
Gabriel Rufino <>
2019-01-26 22:32:52 -0300

    Loader in the component RecognizePicture

--- src/views/RecognizePicture.vue
+++ src/views/RecognizePicture.vue
@@ -1,104 +1,111 @@
 <template>
   <v-container>
-    <v-card>
-      <v-container>
-        <v-tabs
-          centered
-          light
-          icons-and-text
-        >
-          <v-tabs-slider color="primary"></v-tabs-slider>
+    <loader v-if="loadingClassifier"></loader>
+    <div v-else>
+      <v-card>
+        <v-container>
+          <v-tabs
+            centered
+            light
+            icons-and-text
+          >
+            <v-tabs-slider color="primary"></v-tabs-slider>
 
-          <!-- Tabs -->
-          <v-tab>
-            Web
-            <v-icon>language</v-icon>
-          </v-tab>
+            <!-- Tabs -->
+            <v-tab>
+              Web
+              <v-icon>language</v-icon>
+            </v-tab>
 
-          <v-tab>
-            Upload
-            <v-icon>computer</v-icon>
-          </v-tab>
+            <v-tab>
+              Upload
+              <v-icon>computer</v-icon>
+            </v-tab>
 
-          <v-tab>
-            Câmera
-            <v-icon>camera_alt</v-icon>
-          </v-tab>
+            <v-tab>
+              Câmera
+              <v-icon>camera_alt</v-icon>
+            </v-tab>
 
-          <!-- Tabs content -->
-          <v-tab-item>
-            <v-layout row wrap>
-              <v-flex xs9>
-                <v-text-field
-                  prepend-icon="link"
-                  label="Insira a URL da imagem"
-                  v-model="urlImage"
-                >
-                </v-text-field>
-              </v-flex>
-              <v-flex xs1></v-flex>
-              <v-flex xs2>
-                <v-btn
-                  color="primary"
-                  block
-                  large
-                  @click="loadImageFromUrl"
-                >
-                  Carregar
-                </v-btn>
-              </v-flex>
-            </v-layout>
-          </v-tab-item>
+            <!-- Tabs content -->
+            <v-tab-item>
+              <v-layout row wrap>
+                <v-flex xs9>
+                  <v-text-field
+                    prepend-icon="link"
+                    label="Insira a URL da imagem"
+                    v-model="urlImage"
+                  >
+                  </v-text-field>
+                </v-flex>
+                <v-flex xs1></v-flex>
+                <v-flex xs2>
+                  <v-btn
+                    color="primary"
+                    block
+                    large
+                    @click="loadImageFromUrl"
+                  >
+                    Carregar
+                  </v-btn>
+                </v-flex>
+              </v-layout>
+            </v-tab-item>
 
-          <v-tab-item>
-            <input id="image-upload" type="file" hidden @change="onFileChange">
-            <v-btn
-              color="primary"
-              class="white--text"
-              @click.native="openFileDialog"
-            >
-              Upload <v-icon right dark>cloud_upload</v-icon>
-            </v-btn>
-          </v-tab-item>
+            <v-tab-item>
+              <input id="image-upload" type="file" hidden @change="onFileChange">
+              <v-btn
+                color="primary"
+                class="white--text"
+                @click.native="openFileDialog"
+              >
+                Upload <v-icon right dark>cloud_upload</v-icon>
+              </v-btn>
+            </v-tab-item>
 
-          <v-tab-item>
-            Em breve...
-          </v-tab-item>
-        </v-tabs>
-      </v-container>
-    </v-card>
+            <v-tab-item>
+              Em breve...
+            </v-tab-item>
+          </v-tabs>
+        </v-container>
+      </v-card>
 
-    <v-divider class="my-3"></v-divider>
+      <v-divider class="my-3"></v-divider>
 
-    <v-card>
-      <v-container>
-        <img 
-          id="image"
-          :src="image"
-          trasition
-        >
-        <v-btn
-          color="primary"
-          @click="classifyImage"
-          :loading="classifyingImage"
-          :disabled="classifyingImage"
-          block
-        >
-          Classificar imagem
-        </v-btn>
+      <v-card>
+        <v-container>
+          <img 
+            id="image"
+            :src="image"
+            trasition
+          >
+          <v-btn
+            color="primary"
+            @click="classifyImage"
+            :loading="classifyingImage"
+            :disabled="classifyingImage"
+            block
+          >
+            Classificar imagem
+          </v-btn>
 
-        <canvas id="myChart"></canvas>
-      </v-container>
-    </v-card>
+          <canvas id="myChart"></canvas>
+        </v-container>
+      </v-card>
+    </div>
   </v-container>
 </template>
 
 <script>
 import Chart from 'chart.js'
 import ml5 from 'ml5'
+import Loader from '@/components/Loader'
 
 export default {
   name: 'RecognizePicture',
+  components: {
+    Loader
+  },
   data() {
     return {
       classifier: null,

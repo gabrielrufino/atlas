677c7c8073b101e267f6e378be8cd2d68a01505f
Gabriel Rufino <>
2019-01-26 23:21:37 -0300

    Filling with data

--- src/charts/ImagePredictions.js
+++ src/charts/ImagePredictions.js
@@ -5,12 +5,16 @@ export default {
   props: ['predictions'],
   mounted () {
     const data = {
-
+      labels: this.predictions.map(prediction => prediction.className),
+      datasets: [{
+        label: "Certeza da classificação",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: this.predictions.map(prediction => prediction.probability),
+      }]
     }
 
-    const options = {
-
-    }
+    const options = {}
 
     this.renderChart(data, options)
   }

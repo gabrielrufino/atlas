1b16cb871ba50c3ef9fb9bc41cda34f433628c6b
Gabriel Rufino <>
2019-01-26 23:05:52 -0300

    up

--- src/charts/ImagePredictions.js
+++ src/charts/ImagePredictions.js
@@ -3,6 +3,14 @@ import { Bar } from 'vue-chartjs'
 export default {
   extends: Bar,
   mounted () {
+    const data = {
+
+    }
+
+    const options = {
+
+    }
+
     this.renderChart(data, options)
   }
 }

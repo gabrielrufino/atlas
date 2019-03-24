72dee86bda41adbec397b7112933b6f2dbdd6b05
Gabriel Rufino <contato@gabrielrufino.com>
2019-03-24 18:00:28 -0300

    Component to the text summarizer algorithm

---
+++ src/views/TextSummarizer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Loader/>
+  </div>
+</template>
+
+<script>
+import Loader from '@/components/Loader'
+
+export default {
+  name: 'TextSummarizer',
+  components: {
+    Loader
+  }
+}
+</script>

e4e0572a12aa5ed79c3b20da24b2c04a864d1f80
Gabriel Rufino <contato@gabrielrufino.com>
2019-10-09 14:53:55 -0300

    Untrained IA in /views/untrained

---
+++ src/views/untrained/ImageClassifier.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container
+    fluid
+    grid-list-md
+  >
+    <input type="file" />
+    <v-text-field
+      label="Class name"
+      v-model="newClass"
+    ></v-text-field>
+    <v-btn
+      color="primary"
+      @click="addClass"
+    >
+      Add class
+    </v-btn>
+
+    <v-select
+      :items="classes"
+      label="Image class"
+    ></v-select>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'UntrainedImageClassifier',
+  data() {
+    return {
+      newClass: '',
+      classes: [],
+    }
+  },
+  methods: {
+    addClass() {
+      this.classes.push(this.newClass)
+      this.newClass = ''
+    }
+  }
+}
+</script>
---
+++ src/views/untrained/LinearRegression.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container
+    fluid
+    grid-list-md
+  >
+    <Loading />
+    Regressão linear
+  </v-container>
+</template>
+
+<script>
+import Loading from '@/components/Loading'
+
+export default {
+  name: 'LinearRegression',
+  data() {
+    return {
+      training: false,
+    }
+  },
+  mothods: {
+    train() {
+      // Train the model
+    }
+  }
+}
+</script>
